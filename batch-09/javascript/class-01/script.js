d9bf1f86d527a5f137d684fe97e480c70a68d599
Waleed Sheikh <125444427+waleedsheikh30@users.noreply.github.com>
2025-02-08 15:55:48 +0500

    Add files via upload

---
+++ batch-09/javascript/class-01/script.js
@@ -0,0 +1,46 @@
+// JS is a programming Language used to give instructions to computer to perform tasks
+// JS is an dynamically typed language
+
+// data types: (let, var , const)
+
+
+// for print
+console.log("Welcome to JS");
+
+// Varivale declaration
+
+// var is function-scoped
+// allow re-declaration
+var fullName = "Waleed";
+var fullname = 123;
+var fullName = 3.14;
+var fullName = 3.14;
+
+// methods of dcelare a variable name
+let age_ = 123
+let age1 = 123
+let age_2 = 123
+let isRaining$ = true;
+
+// camel case letter
+// best practice
+// let fullName = 3.14;
+
+// PascalCase
+let FullName = 3.14;
+
+console.log(typeof FullName);
+
+
+
+// let is block-scope
+// don't allow re-declaration
+let age = 1234556;
+// let age = "hello";
+
+const pi = 3.14;
+
+
+
+
+// a + b = ?
